6edf70d8ef8a88e75540c55525941a4497f68840
ana.gibson <anaclarasouzagibson@gmail.com>
2025-01-24 15:46:32 -0300

    tentando fazer o método realizar pedido

--- src/Cliente.java
+++ src/Cliente.java
@@ -5,7 +5,7 @@ public class Cliente {
     private String nomeCliente;
     private String telefone;
     private Endereco endereco;
-    private List<Pedido> pratosPedidos;
+    private List<Prato> pratosPedidos;
     Scanner leitura = new Scanner(System.in);
 
     public void exibeMenu() {
@@ -50,8 +50,60 @@ public class Cliente {
     }
 
     private void realizarPedido(){
+        var opcao = -1;
+        while (opcao != 0){
+            var menuPratos = """
+                MENU DE PRATOS:
+                1. Lasanha Bolonhesa
+                2. Macarrão alho e óleo
+                3. Risoto de camarão
+                4. Lasanha 4 queijos
+                
+                0. Finalizar pedido
+                ESCOLHA O PRATO:
+                """;
 
+            System.out.println(menuPratos);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    Prato lasanhaBolonhesa = new Prato();
+                    lasanhaBolonhesa.setNomePrato("Lasanha Bolonhesa");
+                    lasanhaBolonhesa.setDescricao("Lasanha bolonhesa é uma massa em camadas com molho de carne, queijo e bechamel.\n");
+                    lasanhaBolonhesa.setPreco(55.99);
+                    pratosPedidos.add(lasanhaBolonhesa);
+                case 2:
+                    Prato macarao = new Prato();
+                    macarao.setNomePrato("Macarrão Alho e Oléo");
+                    macarao.setDescricao("Macarrão alho e óleo é um prato simples e delicioso, preparado com macarrão salteado em azeite, alho dourado e uma pitada de sal.\n" +
+                            "\n");
+                    macarao.setPreco(30.00);
+                    pratosPedidos.add(macarao);
+                case 3:
+                    Prato risoto = new Prato();
+                    risoto.setNomePrato("Risoto de camarão");
+                    risoto.setDescricao("Risoto de camarão é um prato cremoso com arroz arbóreo, camarões, caldo de legumes, vinho branco e queijo parmesão.");
+                    risoto.setPreco(55.99);
+                    pratosPedidos.add(risoto);
+                    break;
+                case 4:
+                    Prato lasanhaQueijo = new Prato();
+                    lasanhaQueijo.setNomePrato("Lasanha 4 queijos");
+                    lasanhaQueijo.setDescricao("Lasanha 4 queijos é uma massa em camadas com molho cremoso de mozzarella, parmesão, gorgonzola e provolone.");
+                    lasanhaQueijo.setPreco(65.00);
+                    pratosPedidos.add(lasanhaQueijo);
+                    break;
+                case 0:
+                    System.out.println("Finalizando pedido");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        }
     }
+
     private void listarPedido(){
 
     }
--- src/Prato.java
+++ src/Prato.java
@@ -1,4 +1,35 @@
-public record Prato(String nomePrato,
-                    String descricao,
-                    Double preco) {
+public class Prato {
+    private String nomePrato;
+    private String descricao;
+    private Double preco;
+
+    public Prato() {
+        this.nomePrato = nomePrato;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public String getNomePrato() {
+        return nomePrato;
+    }
+
+    public void setNomePrato(String nomePrato) {
+        this.nomePrato = nomePrato;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
 }
